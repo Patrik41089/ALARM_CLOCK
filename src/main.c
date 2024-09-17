91bf78842aad1ed27a5a5499bbc867a30275cfaa
Patrik Radvák <rad41089@spseol.cz>
2024-09-17 18:40:54 +0200

    úpravy

--- src/main.c
+++ src/main.c
@@ -14,13 +14,13 @@ void init(void)
   init_milis();
 
   //I2C
-  I2C_DeInit();
+  /* I2C_DeInit();
   I2C_Init(100000,  //frekvence výstupní pro komunikaci I2C
   0x00,             //(slave) adresa I2C, hodnota mě nezajímá, protože mám mikrokontrolér jako MASTER
   I2C_DUTYCYCLE_2,  //trvání LOW a HIGH (tady to je 1:1, 16:9 je ještě možné vybrat)
   I2C_ACK_CURR,     //povolení signálu, když příjmu data, mikrokontrolér odešle ACK
   I2C_ADDMODE_7BIT, //pro RTC mi stačí 7bit adresa (0x68), možnost ještě 10bit
-  16000000);        //taktování procesoru => mám na 16Mhz
+  16000000);        //taktování procesoru => mám na 16Mhz */
 
   //UART
   UART1_DeInit();
@@ -32,10 +32,11 @@ void init(void)
        UART1_MODE_TXRX_ENABLE         //přijímám a odesílám
        );
 
-  enableInterrupts();                         //globálně povolím přerušení
+  //enableInterrupts();                         //globálně povolím přerušení
   UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE);   //povolí přerušení při příjmu znaku
                                               //RXNE(Receive Data Register Not Empty) přijatý byte dat je připraven k přetečení
                                               //OR(OverRUn) signalizuje přetečení při přijímání dat
+  UART1_Cmd(ENABLE);
 }
 
 //pro funkci printf musím nadeklarovat funkci putchar
@@ -53,30 +54,14 @@ void main(void)
     init();
 
     while (1) {
-      if(milis() - time > 5000)
+      if(milis() - time > 500)
       {
         time = milis();
-        printf("Ahoj");
+        UART1_SendData8('A');
+        printf("AHOJ!");
       }
     }
 }
 
 /*-------------------------------  Assert -----------------------------------*/
 #include "__assert__.h"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
